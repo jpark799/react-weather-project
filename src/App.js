19e8aadd3137637d14bf088381f20aafeb47397f
Jason Park <jason.j.park79@gmail.com>
2019-08-11 23:07:39 -0400

    Weather component made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Titles from './components/Titles'
 import Form from './components/Form'
+import Weather from './components/Weather'
+
 
 class App extends React.Component {
   render() {
     return (
       <div>  
-        <Titles/>
-        <Form/>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     )
   }
