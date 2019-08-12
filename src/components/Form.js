f147cade0d46301f8198fdecd2d85f618a1374ec
Jason Park <jason.j.park79@gmail.com>
2019-08-11 23:05:19 -0400

    Form component made

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div>This is the Form Component</div>
+        )
+    }
+}
+export default Form
