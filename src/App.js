8cc2908c7f605a9a4351638c175ea15a65ea53e9
Jason Park <jason.j.park79@gmail.com>
2019-08-12 00:07:41 -0400

    passing in form values and passing values into api call

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ const API_KEY = `0439b3d5de6bee4961a3e1454084792f`
 class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=gwangju,southkorea&appid=${API_KEY}&units=metric`);
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json()
     console.log(data)
   }
