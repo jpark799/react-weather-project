b22f4935e50d07d2d6861c76671bdf370732ab02
Jason Park <jason.j.park79@gmail.com>
2019-08-11 23:35:10 -0400

    added apikey and call

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import Titles from './components/Titles'
 import Form from './components/Form'
 import Weather from './components/Weather'
 
+const API_KEY = `0439b3d5de6bee4961a3e1454084792f`
 
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=gwangju,southkorea&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json()
+  }
   render() {
     return (
       <div>  
