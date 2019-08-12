75f0660f56d6b938d2480fe933cf4efe3d0f3e00
Jason Park <jason.j.park79@gmail.com>
2019-08-12 00:02:14 -0400

    prevents default refresh

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Weather from './components/Weather'
 const API_KEY = `0439b3d5de6bee4961a3e1454084792f`
 
 class App extends React.Component {
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=gwangju,southkorea&appid=${API_KEY}&units=metric`);
     const data = await api_call.json()
     console.log(data)
