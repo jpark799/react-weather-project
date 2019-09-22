3ab46f44b0a5515bd639313b795fe19e2ad85b2b
jpark799 <jason.j.park79@gmail.com>
2019-09-22 18:23:43 +0900

    added conditional rendering

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,10 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
-                <p>Temperature: {this.props.temperature} </p>
-                <p>Location: {this.props.city}, {this.props.country}</p>
-                <p>Humidity: {this.props.humidity}</p>
-                <p>Conditions: {this.props.description}</p>
+                {this.props.temperature && <p>Temperature: {this.props.temperature} </p>}
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Conditions: {this.props.description}</p>}
             </div>
         )
     }
