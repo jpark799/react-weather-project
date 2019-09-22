5572b9b11e152d59526ed938eadff20bf96de5c8
jpark799 <jason.j.park79@gmail.com>
2019-09-22 17:26:59 +0900

    set state for temp

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends React.Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json()
     console.log(data)
+    this.setState({
+      temperature: data.main.temp,
+      
+    })
   }
   render() {
     return (
