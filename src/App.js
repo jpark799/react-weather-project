1a455b9d81b17cb3a872c29e551b6c2db3f76749
jpark799 <jason.j.park79@gmail.com>
2019-09-22 17:22:47 +0900

    initial state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import Weather from './components/Weather'
 const API_KEY = `0439b3d5de6bee4961a3e1454084792f`
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value
