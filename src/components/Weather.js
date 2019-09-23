aec0f59fec75a12b0672941cfae42416d4370af2
jpark799 <jason.j.park79@gmail.com>
2019-09-23 16:59:00 +0900

    explicit return in weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,14 @@
 import React from "react"
 
-const Weather = (props) => {
-    return (
-        <div>
-            {props.temperature && <p>Temperature: {props.temperature} </p>}
-            {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
-            {props.humidity && <p>Humidity: {props.humidity}</p>}
-            {props.description && <p>Conditions: {props.description}</p>}
-            {props.error && <p>Error: {props.error}</p>}
-        </div>
-    )
-}
+const Weather = props => (
+    <div>
+        {props.temperature && <p>Temperature: {props.temperature} </p>}
+        {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
+        {props.humidity && <p>Humidity: {props.humidity}</p>}
+        {props.description && <p>Conditions: {props.description}</p>}
+        {props.error && <p>Error: {props.error}</p>}
+    </div>
+)
+
 
 export default Weather
