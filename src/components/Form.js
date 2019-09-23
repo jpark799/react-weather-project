db4cdb69d746e0b9e30264c54a0c6bc11e7c0277
jpark799 <jason.j.park79@gmail.com>
2019-09-23 17:26:18 +0900

    converted form component to stateless functional component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,12 @@
 import React from "react"
 
-class Form extends React.Component {
-    render() {
-        return (
-            <form onSubmit={this.props.getWeather}>
-                <input type='text' name='city' placeholder='City...'/>
-                <input type='text' name='country' placeholder='Country...'/>
-                <button>Fetch</button>
-            </form>
-        )
-    }
-}
+const Form = (props) => (
+    <form onSubmit={props.getWeather}>
+        <input type='text' name='city' placeholder='City...'/>
+        <input type='text' name='country' placeholder='Country...'/>
+        <button>Fetch</button>
+    </form> 
+)
+
+
 export default Form
