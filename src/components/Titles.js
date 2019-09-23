12a09f7640968e1bafb9426cbac240398e349c77
jpark799 <jason.j.park79@gmail.com>
2019-09-23 17:12:52 +0900

    converted title component into stateless functional component

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,13 +1,9 @@
 import React from "react"
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1> How's the Weather?</h1>
-            </div>
-        )
-    }
-}
+const Titles = () => (
+    <div>
+        <h1> How's the Weather?</h1>
+    </div>
+)
 
 export default Titles
