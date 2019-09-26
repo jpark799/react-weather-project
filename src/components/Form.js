cb6f28a4431d3ab68aa1d72fb09def684817a19f
jpark799 <jason.j.park79@gmail.com>
2019-09-26 16:18:21 +0900

    imported bootstrap

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Form = (props) => (
+const Form = props => (
     <form onSubmit={props.getWeather}>
         <input type='text' name='city' placeholder='City...'/>
         <input type='text' name='country' placeholder='Country...'/>
